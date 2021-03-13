9bc34a137e50902c462ac22138164ab5613066a6
nabsiek <nabsiek@gmail.com>
2021-03-13 16:13:46 +0000

    week 3

--- unsolved/week-03/day-01/04-Stu_Hello-Variable/Unsolved/script.js
+++ unsolved/week-03/day-01/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,9 @@
 // Write Your JavaScript Code Here
+let studentName = "Nabeela";
+let studentAge = 30;
+
+// reassign
+studentName = "Jack";
+studentAge = 20;
+
+console.log("My name is, " + studentName);
